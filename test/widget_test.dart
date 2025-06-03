e8bc8198cb52f2b4f84963b7a51d3e4d9ac953d8
24Yashwanth24 <kyashwanth831@gmail.com>
2025-06-04 00:43:23 +0530

    fixed widget_test.dart issue

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hope/main.dart';
+import 'package:PayLyt/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
